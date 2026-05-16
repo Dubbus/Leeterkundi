48364b22a6cd9dfaa6930f73cf654c1d741be0bc
Adarsh Muralidharan <165833439+Dubbus@users.noreply.github.com>
2026-05-16 12:44:04 -0400

    Time: 23 ms (5.26%) | Memory: 19.6 MB (56.63%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,16 +1,19 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        l, r = 0, len(s) -1 
+        #initialize the two pointers 
+        l = 0 
+        r = len(s) -1 
 
+        #outer while loop should ensure that l < r 
         while l < r: 
-            while l < r and not self.alphaNum(s[l]): 
+            while l < r and not self.alphaNum(s[l]):
                 l += 1
-            while r > l and not self.alphaNum(s[r]): 
-                r -= 1    
-            if s[l].lower() != s[r].lower(): 
+            while r > l and not self.alphaNum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
                 return False
-            l, r = l + 1, r -1 
-        return True 
+            l, r = l + 1, r - 1
+        return True
 
     def alphaNum(self, c): 
         return (ord('A') <= ord(c) <= ord('Z') or
